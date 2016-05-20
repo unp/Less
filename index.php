790e30692cc87fb041aeb68fcb50a7ea73c631e5
Umang Patel <umang@paytango.com>
2016-05-20 15:04:26 -0700

    updated logo

--- index.php
+++ index.php
@@ -24,16 +24,8 @@
 <header id="masthead" class="site-header" role="banner">
 	<div class="container">
 		
-		<div class="gravatar">
-			<?php 
-				// grab admin email and their photo
-				$admin_email = get_option('admin_email');
-				echo get_avatar( $admin_email, 100 ); 
-			?>
-		</div><!--/ author -->
-		
 		<div id="brand">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a> &mdash; <span><?php echo get_bloginfo( 'description' ); ?></span></h1>
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="https://s3-us-west-1.amazonaws.com/cloverbook-wp/logo.png" /></a></h1>
 		</div><!-- /brand -->
 	
 		<nav role="navigation" class="site-navigation main-navigation">
